1b6b68585e15050c0a91be5f3d75b60e41aa3a3c
webbo <e.bonardi@me.com>
2023-07-21 14:04:33 +0200

    added logout confirmation via modal via htmx for #1

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import CustomUser, TravelFriend
 
 
 def account_profile(request):
-    context = {}
+    user = get_object_or_404(CustomUser, email=request.user)
+    travel_friends = TravelFriend.objects.filter(user=user)
+    context = {
+        "user": user,
+        "travel_friends": travel_friends,
+    }
     return render(request, "account/profile.html", context)
--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,12 @@ function userScroll() {
 }
 
 document.addEventListener('DOMContentLoaded', userScroll);
+
+const modal = new bootstrap.Modal(document.getElementById("modal"))
+
+htmx.on("htmx:afterSwap", (e) => {
+  // Response targeting #dialog => show the modal
+  if (e.detail.target.id == "dialog") {
+    modal.show()
+  }
+})
